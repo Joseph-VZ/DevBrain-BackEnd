a28a51dff385fd348f1fc8165a1911d422c7ddba
ame-97 <2311082007@alumno.utpuebla.edu.mx>
2026-07-03 17:28:19 -0600

    fix: index

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import { conectarBaseDatos } from "./config/database.js";
 
+import authRoutes from "./routes/auth.routes.js";
+import projectRoutes from "./routes/project.routes.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+// RUTAS
+app.use("/auth", authRoutes);
+app.use("/projects", projectRoutes);
 
 // HEALTH CHECK
 app.get("/health", (req, res) => {
@@ -21,3 +28,7 @@ conectarBaseDatos();
 app.listen(PORT, () => {
     console.log(`DevBrain backend running on port ${PORT}`);
 });
+
+// DEBUG (opcional)
+console.log("DATABASE_URL =>", process.env.DATABASE_URL);
+console.log("JWT_SECRET =>", process.env.JWT_SECRET);
